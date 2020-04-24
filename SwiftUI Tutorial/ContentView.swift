d4bcd07b8579510f9cab1e7b6d8132165b62df94
Azat <kaumov.a.r@gmail.com>
2020-04-24 19:25:26 +0300

    Initial Commit

---
+++ SwiftUI Tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Tutorial
+//
+//  Created by Azat Kaiumov on 24.04.2020.
+//  Copyright © 2020 Azat Kaiumov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
